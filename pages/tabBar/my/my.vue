2f23323b9ae835d535d60f2de609665f971d2c77
zxx <zxx@heimaoba.cn>
2022-11-17 17:41:15 +0800

    编写我的页面

--- pages/tabBar/my/my.vue
+++ pages/tabBar/my/my.vue
@@ -1,22 +1,32 @@
 <template>
-	<view>
-		我的
+	<view class="container">
+		<image :src="bgImageURL"
+			mode="aspectFill"
+			style="width: 100%; height: 100%; background-color: #ffffff;position: absolute;">
+		</image>
+		<view class="my-info">
+			<view class="my-head">
+				<u-image :src="headImageURL"
+					shape="circle"></u-image>
+			</view>
+		</view>
 	</view>
 </template>
-
 <script>
 	export default {
 		data() {
 			return {
-				
+				bgImageURL: 'https://me.heimaoba.cn/static/image/mybg.png',
+				headImageURL: 'https://me.heimaoba.cn/static/image/oneqrbg.png',
 			}
 		},
-		methods: {
-			
-		}
+		methods: {}
 	}
 </script>
-
-<style>
-
+<style lang="scss"
+	scoped>
+	.container {
+		background-color: #ffffff;
+		height: 100%;
+	}
 </style>
