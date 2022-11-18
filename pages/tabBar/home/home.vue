7148e3389905d0f2032e0819fe15e118d65209a9
zxx <zxx@heimaoba.cn>
2022-11-18 16:17:35 +0800

    首页增加分享功能

--- pages/tabBar/home/home.vue
+++ pages/tabBar/home/home.vue
@@ -213,6 +213,15 @@
 				zZhiweiData: homeData.zZhiweiData.data.list,
 			}
 		},
+		onShareAppMessage(res) {
+			if (res.from === 'button') { // 来自页面内分享按钮
+				console.log(res.target)
+			}
+			return {
+				title: 'testHr',
+				path: 'pages/tabBar/home/home'
+			}
+		},
 		methods: {
 			openShowSearch() {
 				this.showSearch = true;
