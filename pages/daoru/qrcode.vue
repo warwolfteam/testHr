d527b412ca654af28d31571c13212c5f4704605d
zxx <zxx@heimaoba.cn>
2022-11-21 17:40:59 +0800

    新增候选人扫码页

---
+++ pages/daoru/qrcode.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="container">
+		<view class="paixu">
+			<view class="wangzhi"> {{qrcodeUrl}} </view>
+			<view class="miaoshu"> 请保存该二维码 </view>
+			<view class="anniu">
+				<u-button type="warning"
+					size="medium"
+					@click="copy">保存二维码</u-button>
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				qrcodeUrl: "www.testhr.com"
+			}
+		},
+		methods: {
+			copy() {
+				uni.setClipboardData({
+					data: this.qrcodeUrl,
+					success: function() {
+						console.log('success');
+					}
+				});
+			}
+		}
+	}
+</script>
+<style lang="scss"
+	scoped>
+	.container {
+		background-color: #ffffff;
+		height: 100vh;
+
+		.paixu {
+			padding-top: 25%;
+			height: 60%;
+			width: 100%;
+			padding: 20%;
+			text-align: center;
+
+			.icon {
+				margin: 30rpx;
+			}
+
+			.wangzhi {
+				margin-top: 20rpx;
+				font-size: 40rpx;
+				font-weight: 1000;
+			}
+
+			.miaoshu {
+				margin-top: 20rpx;
+			}
+
+			.anniu {
+				margin-top: 40rpx;
+			}
+		}
+	}
+</style>
